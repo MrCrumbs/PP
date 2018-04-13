d45b40ccdaa912129250fffe28ab3570adf4e4ee
Jonathan <pc.crumbs@gmail.com>
2018-04-13 14:20:43 +0300

    Step 3 done

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from O365 import Inbox
 from datetime import datetime, timedelta
-import argparse, json, re, requests, os, boto3
+import argparse, json, re, requests, os, boto3, botocore, csv, pandas
 USERNAME = "qa.ex@office365.ecknhhk.xyz"
 PASSWORD = "ew68I7W52p*W"
 BUCKET_NAME = "interview-exercises"
@@ -8,9 +8,9 @@ BUCKET_NAME = "interview-exercises"
 # main flow
 def main(args):
     # connect to inbox and download messages
-    inbox = login_and_download(USERNAME, PASSWORD) 
+    inbox = login_and_download(USERNAME, PASSWORD, args.report)
     # upload to s3 or download locally
-    save_emails(inbox, args.dir)
+    save_emails(inbox, args.dir, args.report)
 
 
 # handle args
@@ -18,6 +18,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", help="input directory for email download")
     parser.add_argument("--upload", help="upload emails to AWS S3", action="store_true")
+    parser.add_argument("--report", help="generate email report", action="store_true")
     args = parser.parse_args()
     # just making sure either --dir or --upload was used correctly
     if args.upload:
@@ -33,7 +34,7 @@ def parse_arguments():
 
 
 # connect to inbox and download messages
-def login_and_download(username, password):
+def login_and_download(username, password, report):
     auth = (username, password)
     inbox = Inbox(auth, getNow=False)
     # define 24-hour filter
@@ -50,6 +51,9 @@ def login_and_download(username, password):
     except requests.ConnectionError:
         print("Connection error during download. Exiting...")
         quit()
+    # generate report of downloaded emails, for now without s3 link
+    if report:
+        generate_report(inbox.messages)
     # handle errors connecting/download
     if result:
         return inbox
@@ -58,7 +62,7 @@ def login_and_download(username, password):
         quit()
 
 # iterate through emails and store them appropriately
-def save_emails(inbox, path):
+def save_emails(inbox, path, report):
     index = 0
     for email in inbox.messages:
         # create filename for email
@@ -66,7 +70,7 @@ def save_emails(inbox, path):
         index += 1
         # either upload to s3 or store locally
         if args.upload:
-            upload(email, filename)
+            upload(email, filename, report, index)
         else:
             save_locally(email, filename, path)
     print("Done. Stored " + str(index) + " emails.")
@@ -86,21 +90,46 @@ def save_locally(email, filename, path):
 
 
 # upload email to s3
-def upload(email, filename):
+def upload(email, filename, report, index):
     filename += ".txt"
     client = boto3.client("s3")
+    # write email to s3, and update report with url
     try:
         response = client.put_object(Bucket=BUCKET_NAME,
                                      Body=json.dumps(email.json),
                                      Key=filename)
+        if report:
+            url = client.generate_presigned_url("get_object", 
+                                                Params = {"Bucket": BUCKET_NAME, 
+                                                          "Key": filename})
+            update_url_in_report(url, index)
     except client.exceptions.NoSuchBucket:
         print("Bucket doesn't exist. Exiting...")
         quit()
-    except client.exceptions.EndpointConnectionError:
+    except (botocore.exceptions.EndpointConnectionError,
+            botocore.vendored.requests.exceptions.ConnectionError):
         print("Connection error during upload. Exiting...")
         quit()
 
 
+# generate email report, for now without s3 link
+def generate_report(emails):
+    writer = csv.writer(open("report.csv", "w"))
+    writer.writerow(["Time", "From", "To", "Subject", "S3 Link"])
+    for email in emails:
+        writer.writerow([email.json["DateTimeReceived"].encode('utf-8'), 
+                        email.json["Sender"]["EmailAddress"]["Address"].encode('utf-8'), 
+                        email.json["ToRecipients"][0]["EmailAddress"]["Address"].encode('utf-8'), 
+                        email.json["Subject"].encode('utf-8'),
+                        "Not uploaded".encode('utf-8')])
+
+
+# when email is uploaded to s3 - update report with link
+def update_url_in_report(url, index):
+    df = pandas.read_csv("report.csv")
+    df.iat[index-1, 4] = url
+    df.to_csv("report.csv", index=False)
+    
 if __name__ == "__main__":
     args = parse_arguments()
     main(args)
